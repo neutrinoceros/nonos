a2402b9647cd42d0c3635224e66622a6baeb83de
Clément Robert <cr52@protonmail.com>
2026-02-14 22:42:41 +0100

    TYP: simplify type annotations on `find_*` funcs

--- src/nonos/api/tools.py
+++ src/nonos/api/tools.py
@@ -3,13 +3,13 @@ import numpy as np
 from nonos._types import D, F, FArray
 
 
-def find_nearest(array: FArray[D, F], value: float | F) -> int:
+def find_nearest(array: FArray[D, F], value: float) -> int:
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return int(idx)
 
 
-def find_around(array: FArray[D, F], value: float | F) -> FArray[D, F]:
+def find_around(array: FArray[D, F], value: float) -> FArray[D, F]:
     array = np.asarray(array)
     idx_1 = (np.abs(array - value)).argmin()
     larray = list(array)
